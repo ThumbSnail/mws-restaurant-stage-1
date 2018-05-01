cd0d62c5918c1508a87a3d613d398d541a9f83f4
ThumbSnail <thumbsnail@zoho.com>
2018-05-01 15:45:08 -0700

    fix:  Added ARIA labels, checked semantics

--- js/main.js
+++ js/main.js
@@ -167,6 +167,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', 'View details about ' + restaurant.name);
   div_container.append(more);
 
   return li;
