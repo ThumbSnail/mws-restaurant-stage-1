773b7a563ce0f8a8194c61f91f0c173145d2bbb5
ThumbSnail <thumbsnail@zoho.com>
2018-05-08 16:55:25 -0700

    revamp: Continuing the switch to model-view-controller, Model and View done

--- js/allMain.js
+++ js/allMain.js
@@ -5,17 +5,57 @@ const model, view, controller;
 class Model {
   constructor() {
     this._arrRestaurants = [];
+    this._arrCuisineOptions = [];
+    this._arrNeighborhoodOptions = [];
   }
 
-  getRestaurants() {
-    return this._arrRestaurants;
+  /*
+   * Arguments:
+   * restaurants = array of Restaurant objects, obtained from network or database via the Controller
+  */
+  addFetchedRestaurants(restaurants) {
+    this._arrRestaurants = restaurants;
   }
 
-  //NEED to have model create the URL for an image and save it as a new property on the restaurant object
-  //AND also the URL for the restaurant's individual page
+  /*
+   * The View expects each restaurant to have an imgUrl and url property, so create those here
+   */
+  addUrlsToRestaurants() {
+    this._arrRestaurants.map(function(element) {
+      element.imgUrl = `/img/${element.photograph}`;
+      element.url = `./restaurant.html?id=${element.id}`;
+    });
+  }
 
-  /*loop through all the restaurants to find unique neighborhoods, cuisine, etc., save in variable*/
+  /*
+   * Arguments:
+   * propertyName = the property in the Restaurant object that corresponds to the desired select filter,
+   * so 'cuisine_type' or 'neighborhood'
+   */
+  setArrOptions(propertyName) {
+    array = this._arrNeighborhoodOptions;
+    if (propertyName === 'cuisine_type') {
+      array = this._arrCuisineOptions;
+    }
 
+    // Get the different values of the property from all restaurants
+    const arrValues = this._arrRestaurants.map((obj) => obj.propertyName);
+    // Remove duplicates from the array
+    const uniqueValues = arrValues.filter((value, index) => arrValues.indexOf(v) == index);
+    array = uniqueValues;
+  }
+
+  getRestaurantsByCuisineAndNeighborhood(cuisine, neighborhood) {
+    let restaurants = this._arrRestaurants;
+    if (cuisine != 'all') {  //then filter by cuisine
+      restaurants = restaurants.filter(r => r.cuisine_type == cuisine);
+    }
+    if (neighborhood != 'all') {  //then filter by neighborhood
+      restaurants = restaurants.filter(r => r.neighborhood == neighborhood);
+    }
+
+    return restaurants;
+  }
 }
 
   /*** Restaurant Object
@@ -45,7 +85,8 @@ class View {
     /*** Restaurant Entries in Flexbox ***/
     this._restaurantsList = document.getElementById('restaurants-list');
     /*** Google Map ***/
-    this._map = document.getElementById('map');
+    this._map = '';
+    this._mapDiv = document.getElementById('map');
     this._mapMarkers = [];
 
     this._displayedRestaurants = [];  //which restaurants are currently displayed
@@ -146,7 +187,7 @@ class View {
       lat: 40.722216,
       lng: -73.987501
     };
-    this._map = new google.maps.Map(mapElem, {
+    this._map = new google.maps.Map(this._mapDiv, {
       zoom: 12,
       center: loc,
       scrollwheel: false
@@ -182,9 +223,78 @@ class View {
 }
 
 
+//controller:
+//open the database
+//fetch all the JSON data (either from database or network), give to model
+    //service worker and have it start caching data  (unless, would waiting to do this later speed up initial page load?)
+//add new properties to each restaurant in the model
+//filter the data for the select fields
+  //all data now obtained
+//fill select fields
+//get filtered restaurants from the model with data from view's getSelectedOption(selectField), pass to view's setDisplayedRestaurants
+//handle map stuff, adding markers
+
+
 /*** Initial Setup ***/
+
+// !!!!! Yeah, could this be changed to make it better/faster?
+// Start fetching the data immediately (even before the DOM loads)
+// can you open the database right away?
+// when can you start caching via the Cache API?
+// so, make a model and a controller rightaway
+// view needs to wait until the DOM exists (is that what DOMContentLoaded is, or does that wait even longer)
+// service worker seems to need to be later, but is that correct?
 document.addEventListener('DOMContentLoaded', function(event) {
   model = new Model();
   view = new View();
   controller = new Controller();
 }
+
+
+
+
+
+//possible controller stuff:
+/* With internet connection, when Google Maps loads, its init event triggers loading page content (since
+ * the map requires some of the restaurant data).  However, without internet, no content would display since
+ * Google Maps doesn't call its init function in that case.  The service worker is set up to detect that 
+ * failed fetch event and then message the client.  But... sometimes the client's listener doesn't load 
+ * fast enough to receive that message.  So this boolean is part of a backup plan to still display content
+ * in that situation.
+ */
+let isContentLoaded = false;  //set true in updateRestaurants()
+let isMapLoaded = false;  //used to determine if a map exists and thus should have markers placed on it
+
+/**
+ * Set up the service worker
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (!navigator.serviceWorker) return;
+
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
+    console.log('ServiceWorker registration failed: ', err);
+  });
+
+  //for receiving a message from the service worker that google maps didn't load (when offline):
+  navigator.serviceWorker.addEventListener('message', event => {
+    if (event.data.msg == 'Google Maps failed') {
+      console.log('Received msg from serviceworker: ' + event.data.msg);
+      if (isContentLoaded) return;
+      updateRestaurants();
+    }
+  });
+
+  fetchNeighborhoods();
+  fetchCuisines();
+
+  //sometimes the service worker posts its message before the client is ready to receive
+  //in that case, include this as a failsafe so that the list of restaurants automatically appears when offline
+  setTimeout(function() {
+    if (isContentLoaded) return;
+
+    console.log('backup timeout called to ensure data loaded');
+    updateRestaurants();
+  }, 1000);
+});
